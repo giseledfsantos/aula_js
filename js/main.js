d6304586cb2facdd80428958425941945fb471dc
Gisele D. F. Santos <zelegz@hotmail.com>
2021-04-03 21:55:02 -0300

    commit inicial - Aula JS

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function btnclick(){
+    document.getElementById("cliquei").innerHTML = "Clique aqui para estudar!";
+}
+
+function redirecionar(){
+    window.open("https://web.digitalinnovation.one/");
+  //  window.location.href = "https://web.digitalinnovation.one/";
+}
+
+function cbochange(elemento){
+    alert(elemento.value);
+}
